4a7035282dd7194b30d8792a6c2ea4de96f12ff7
tndh2002 <omgrightmeow091@gmail.com>
2021-03-25 23:19:04 +0700

    add new folder

---
+++ ConsoleApplication2/ConsoleApplication2.cpp
@@ -0,0 +1,18 @@
+// ConsoleApplication2.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+
+#include <iostream>
+using namespace std;
+
+
+
+struct Diem
+{
+    double toan;
+    double van;
+};
+
+int main()
+{
+    
+}
